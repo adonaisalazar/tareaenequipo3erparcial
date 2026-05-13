6753713d96c69bac658c1eaf63e42174a0be1009
adonaisalazar <adonaisalazar93@gmail.com>
2026-05-13 11:36:56 -0800

    Update teatro.py

--- teatro.py
+++ teatro.py
@@ -1,1 +1,2 @@
-#colegio de estudios cientificos y tecnologicos#
+"""un teatro ortorga descuentos segun la edad del cliente. Determina la cantidad de dinmero que el teatro deja de percibiur por cada una de las categorias. 
+Tomar encuenta que los niños menores de 5 año
