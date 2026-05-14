167c7eacfef71b35aabcdabc981d5862c8605dc9
Sorianojz <sorianojz234@gmail.com>
2026-05-14 07:57:23 -0600

    Update teatro.py

--- teatro.py
+++ teatro.py
@@ -1,2 +1,9 @@
-"""un teatro ortorga descuentos segun la edad del cliente. Determina la cantidad de dinmero que el teatro deja de percibiur por cada una de las categorias. 
-Tomar encuenta que los niños menores de 5 años
+"""Un teatro ortorga descuentos segun la edad del cliente. Determina la cantidad de dinmero que el teatro deja de percibiur por cada una de las categorias.
+Tomar encuenta que los niños menores de 5 años no pueden entrar al teatro y que existe un precio único en los asientos. Los descuentos se hacen tomando en 
+cuenta el siguiente cuadro:
+                  Edad            Descuento
+Categoria 1      5 - 14             35%
+Categoria 2     15 - 19             25%
+Categoria 3     20 - 45             10%
+Categoria 4     46 - 65             25%
+Categoria 5   65 en adelante        35%
